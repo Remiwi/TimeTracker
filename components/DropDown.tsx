4942f7067864797c30c3cc5bd0d98cc01e390c0d
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-06-22 12:06:51 -0400

    Forgot to commit components

---
+++ components/DropDown.tsx
@@ -0,0 +1,85 @@
+import { MaterialIcons } from "@expo/vector-icons";
+import { useState } from "react";
+import { Text, TouchableNativeFeedback, View } from "react-native";
+
+export default function MyDropDown(props: {
+  label: string;
+  options: string[];
+  value?: string;
+  onChange?: (text: string) => void;
+  bgColor?: string;
+  labelColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  placeholderColor?: string;
+  className?: string;
+}) {
+  const [optionsShown, setOptionsShown] = useState(false);
+
+  return (
+    <View className={props.className}>
+      <View className="relative">
+        <View
+          className="rounded-md border-2 border-slate-500"
+          style={[
+            props.borderColor
+              ? {
+                  borderColor: props.borderColor,
+                }
+              : {},
+          ]}
+        >
+          <Text
+            className="absolute -top-3 left-2 z-10 rounded-sm bg-gray-50 px-1 text-sm text-slate-600"
+            style={[
+              props.labelColor
+                ? {
+                    color: props.labelColor,
+                  }
+                : {},
+              props.bgColor
+                ? {
+                    backgroundColor: props.bgColor || "undefined",
+                  }
+                : {},
+            ]}
+          >
+            {props.label}
+          </Text>
+          <View className="overflow-hidden rounded-sm">
+            <TouchableNativeFeedback
+              onPress={() => setOptionsShown(!optionsShown)}
+            >
+              <View className="flex flex-row items-center justify-between p-2">
+                <Text className="py-1">{props.value}</Text>
+                <MaterialIcons
+                  name="keyboard-arrow-down"
+                  size={24}
+                  color={props.borderColor || "aaaaaa"}
+                  className="h-8 w-8"
+                />
+              </View>
+            </TouchableNativeFeedback>
+          </View>
+        </View>
+        {optionsShown && (
+          <View className="absolute top-14 w-full rounded-b-md bg-gray-50">
+            {props.options.map((option) => (
+              <TouchableNativeFeedback
+                onPress={() => {
+                  setOptionsShown(false);
+                  if (props.onChange) props.onChange(option);
+                }}
+                key={option}
+              >
+                <View className="p-4">
+                  <Text>{option}</Text>
+                </View>
+              </TouchableNativeFeedback>
+            ))}
+          </View>
+        )}
+      </View>
+    </View>
+  );
+}
---
+++ components/TextInput.tsx
@@ -0,0 +1,53 @@
+import { Text, View, TextInput } from "react-native";
+
+export default function StyledTextInput(props: {
+  label: string;
+  placeholder?: string;
+  value?: string;
+  onChange?: (text: string) => void;
+  bgColor?: string;
+  labelColor?: string;
+  borderColor?: string;
+  textColor?: string;
+  placeholderColor?: string;
+  className?: string;
+}) {
+  return (
+    <View className={props.className}>
+      <View
+        className="relative rounded-md border-2 border-slate-500 p-2"
+        style={[
+          props.borderColor
+            ? {
+                borderColor: props.borderColor,
+              }
+            : {},
+        ]}
+      >
+        <Text
+          className="absolute -top-3 left-2 bg-gray-50 px-1 text-sm text-slate-600"
+          style={[
+            props.labelColor
+              ? {
+                  color: props.labelColor,
+                }
+              : {},
+            props.bgColor
+              ? {
+                  backgroundColor: props.bgColor || "undefined",
+                }
+              : {},
+          ]}
+        >
+          {props.label}
+        </Text>
+        <TextInput
+          placeholder={props.placeholder}
+          placeholderTextColor={props.placeholderColor || "#aaaaaa"}
+          value={props.value}
+          onChangeText={props.onChange}
+        />
+      </View>
+    </View>
+  );
+}
