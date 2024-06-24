a8b255ddb2af700500f7f5056de44d15aaa8c569
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-06-24 18:19:30 -0400

    Added UI to projects page

--- app/projects.tsx
+++ app/projects.tsx
@@ -1,5 +1,156 @@
-import { Text } from "react-native";
+import Toggl from "@/apis/toggl";
+import BottomSheet from "@/components/BottomSheet";
+import ColorSelector from "@/components/ColorSelector";
+import MyDropDown from "@/components/DropDown";
+import StyledTextInput from "@/components/TextInput";
+import ColorMaps from "@/utils/colorMaps";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+import {
+  FlatList,
+  Modal,
+  Text,
+  TouchableNativeFeedback,
+  View,
+} from "react-native";
+
+type ProjectStuff = {
+  id: number;
+  name: string;
+  color: string;
+  icon: string;
+};
 
 export default function Page() {
-  return <Text>Projects</Text>;
+  const [projectModalOpen, setProjectModalOpen] = useState(false);
+  const [selectedProject, setSelectedProject] = useState<
+    ProjectStuff | undefined
+  >();
+
+  const projectsQuery = useQuery({
+    queryKey: ["projects"],
+    queryFn: Toggl.getProjects,
+  });
+
+  return (
+    <>
+      {projectModalOpen && (
+        <ProjectModal
+          onCancel={() => setProjectModalOpen(false)}
+          defaultProject={selectedProject}
+        />
+      )}
+      <FlatList
+        data={projectsQuery.data}
+        renderItem={({ item }) => (
+          <Project
+            project={{ ...item, icon: "" }}
+            onPress={() => {
+              setSelectedProject({ ...item, icon: "" });
+              setProjectModalOpen(true);
+            }}
+          />
+        )}
+      />
+    </>
+  );
+}
+
+function Project(props: { project: ProjectStuff; onPress?: () => void }) {
+  return (
+    <TouchableNativeFeedback onPress={props.onPress}>
+      <View className="flex flex-row items-center gap-6 border-b border-gray-100 p-4">
+        <View
+          className="h-12 w-12 rounded-full"
+          style={{ backgroundColor: props.project.color }}
+        />
+        <Text className="text-lg">{props.project.name}</Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
+}
+
+function ProjectModal(props: {
+  onCancel?: () => void;
+  onDone?: (p: ProjectStuff) => void;
+  defaultProject?: ProjectStuff;
+}) {
+  const [name, setName] = useState(props.defaultProject?.name || "");
+  const [color, setColor] = useState(
+    ColorMaps.TogglToInternalTailwind.get(
+      props.defaultProject?.color.toUpperCase() || "#0B83D9",
+    ) || "bg-blue-500",
+  );
+  const [icon, setIcon] = useState(props.defaultProject?.icon || "");
+
+  const iconOptions = [
+    "music-note",
+    "laptop",
+    "dumbbell",
+    "filmstrip",
+    "book",
+    "shopping",
+    "wizard-hat",
+  ];
+
+  return (
+    <BottomSheet onClose={props.onCancel}>
+      <View className="px-4">
+        <View className="flex w-full flex-row items-center justify-between px-2 pb-8">
+          <View className="overflow-hidden rounded-full shadow-sm shadow-slate-900">
+            <TouchableNativeFeedback onPress={props.onCancel}>
+              <View className="flex w-32 items-center rounded-full bg-gray-100 p-3">
+                <Text>Cancel</Text>
+              </View>
+            </TouchableNativeFeedback>
+          </View>
+          <View className="overflow-hidden rounded-full shadow-sm shadow-slate-900">
+            <TouchableNativeFeedback
+              onPress={() => {
+                props.onDone?.({
+                  id: props.defaultProject?.id || -1,
+                  color,
+                  icon,
+                  name,
+                });
+              }}
+            >
+              <View className="flex w-32 items-center rounded-full bg-slate-200 p-3">
+                <Text>Done</Text>
+              </View>
+            </TouchableNativeFeedback>
+          </View>
+        </View>
+        <StyledTextInput
+          label="Project Name"
+          bgColor="white"
+          className="pb-4"
+          value={name}
+          onChange={setName}
+        />
+        <View className="flex flex-row items-center gap-4 pb-2">
+          <ColorSelector value={color} onChange={setColor}>
+            <MaterialCommunityIcons
+              name={icon as any}
+              size={20}
+              color="white"
+            />
+          </ColorSelector>
+          <MyDropDown
+            className="pb flex-grow"
+            options={iconOptions}
+            value={icon}
+            onChange={setIcon}
+            placeholder="Icon"
+          />
+        </View>
+        <View className="w-full items-center justify-center">
+          <Text className="p-4 text-lg font-bold color-red-600">
+            Delete Project
+          </Text>
+        </View>
+      </View>
+    </BottomSheet>
+  );
 }
