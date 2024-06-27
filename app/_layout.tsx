93dfdeb417ff1b4a3680eceb92881b85acf5f53f
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-06-27 16:16:18 -0400

    Removed unneeded hooks

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -14,14 +14,12 @@ import { Drawer } from "expo-router/drawer";
 
 const qc = new QueryClient();
 
-import { useColorScheme } from "@/hooks/useColorScheme";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
   const [loaded] = useFonts({
     SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
   });
@@ -39,10 +37,7 @@ export default function RootLayout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <QueryClientProvider client={qc}>
-        <ThemeProvider
-          value={DefaultTheme}
-          // value={colorScheme === "dark" ? DarkTheme : DefaultTheme}
-        >
+        <ThemeProvider value={DefaultTheme}>
           <Drawer
             screenOptions={{
               headerShown: true,
