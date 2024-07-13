bf1e0b75d001143a5df66eeea39295c555baa301
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-07-13 14:53:11 -0400

    Mutations now invalidate queries based on mutationkey

--- apis/queryclient.ts
+++ apis/queryclient.ts
@@ -1,3 +1,9 @@
-import { QueryClient } from "@tanstack/react-query";
+import { QueryClient, MutationCache } from "@tanstack/react-query";
 
-export const qc = new QueryClient();
+export const qc = new QueryClient({
+  mutationCache: new MutationCache({
+    onSettled: (_data, _error, _variables, _context, mutation) => {
+      qc.invalidateQueries({ queryKey: mutation.options.mutationKey });
+    },
+  }),
+});
