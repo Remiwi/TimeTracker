db877c058121631ec8d2f219a54cac2b6230b5f1
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-07-19 17:42:24 -0400

    Removed memo from paginated... not sure what is was doing in the first place ngl

--- components/Paginated.tsx
+++ components/Paginated.tsx
@@ -1,21 +1,19 @@
 import { usePanHandlers } from "@/hooks/animtedHooks";
-import React, { useRef } from "react";
+import React, { useRef, useMemo } from "react";
 import {
   Animated,
-  PanResponder,
   View,
   useAnimatedValue,
   useWindowDimensions,
 } from "react-native";
 
-function Paginated<T>(props: {
+export default function Paginated<T>(props: {
   pages: T[];
   renderPage: (page: T, index: number) => React.ReactNode;
   onPageChange?: (page: number) => void;
   minPage?: number;
   maxPage?: number;
   setPageToRef?: React.MutableRefObject<((page: number) => void) | null>;
-  dependencies?: any[];
 }) {
   const screen = useWindowDimensions();
   const page = useRef(0);
@@ -98,21 +96,3 @@ function Paginated<T>(props: {
     </View>
   );
 }
-
-function propsAreEqual(
-  prev: { dependencies?: any[] },
-  next: { dependencies?: any[] },
-) {
-  if (prev.dependencies === undefined || next.dependencies === undefined) {
-    return false;
-  }
-  if (prev.dependencies.length !== next.dependencies.length) {
-    throw "dependencies length must be the same";
-  }
-  for (let i = 0; i < prev.dependencies.length; i++) {
-    if (prev.dependencies[i] !== next.dependencies[i]) return false;
-  }
-  return true;
-}
-
-export default React.memo(Paginated, propsAreEqual) as typeof Paginated;
