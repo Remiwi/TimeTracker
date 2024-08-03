19c8e47583e536c697e43f745de421ecc086c98c
Remi <70982674+Remi-NYU@users.noreply.github.com>
2024-08-03 11:14:56 -0400

    Upgraded template modal project list

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,9 @@
-import { useState } from "react";
+import { useRef, useState } from "react";
 import { Modal, Text, TouchableNativeFeedback, View } from "react-native";
 import MyDropDown from "@/components/DropDown";
 import MyTextInput from "@/components/TextInput";
 import MyTagInput from "@/components/TagInput";
-import { Template } from "@/apis/types";
+import { Project, Template } from "@/apis/types";
 import { EntryEditorSheet } from "@/components/EntryEditorSheet";
 import {
   useAddTemplateMutation,
@@ -12,6 +12,8 @@ import {
 } from "@/hooks/templateQueries";
 import { useProjects } from "@/hooks/projectQueries";
 import Templates from "@/components/index/Templates";
+import ListModal from "@/components/ListModal";
+import { Icon } from "@/components/Icon";
 
 export default function Screen() {
   const [templatesEnabled, setTemplatesEnabled] = useState(true);
@@ -88,6 +90,14 @@ function TemplateEditModal(props: {
   onDelete: (id: number) => void;
   defaultTemplate?: Template;
 }) {
+  const [projModalVisible, setProjModalVisible] = useState(false);
+  const projButtonLayout = useRef({
+    x: 0,
+    y: 0,
+    width: 0,
+    height: 0,
+  });
+
   const projectsQuery = useProjects();
 
   const [name, setName] = useState(props.defaultTemplate?.name || "");
@@ -163,17 +173,96 @@ function TemplateEditModal(props: {
             onChange={setDescription}
             className="pb-2"
           />
-          <MyDropDown
-            placeholder="Select Project"
-            options={projectsQuery.data || []}
-            value={projectsQuery.data?.find((p) => p.id === project_id)}
-            onChange={(item) => {
-              setProjectID(item.id);
+          <ListModal
+            options={[null, ...(projectsQuery.data ?? [])]}
+            visible={projModalVisible}
+            backgroundColor="#f0f0f0"
+            height={300}
+            positionRelativeTo={projButtonLayout.current}
+            renderOption={(option: Project | null) => {
+              const proj = option ?? {
+                id: null,
+                name: "No Project",
+                color: "#666",
+                icon: "",
+              };
+
+              return (
+                <View className="flex flex-row gap-4 border-b border-slate-300 px-4 py-2">
+                  <View
+                    className="flex h-8 w-8 items-center justify-center rounded-full"
+                    style={{
+                      backgroundColor:
+                        proj.id === null ? "#00000000" : proj.color,
+                    }}
+                  >
+                    <Icon name={proj.icon as any} color={"white"} size={16} />
+                  </View>
+                  <Text className="text-xl" style={{ color: proj.color }}>
+                    {proj.name}
+                  </Text>
+                </View>
+              );
             }}
-            itemToString={(item) => item.name}
-            className="z-40 pb-2"
-            placeholderColor={projectsQuery.isError ? "#884444" : undefined}
+            onClose={() => setProjModalVisible(false)}
+            onSelect={(selected: Project | null) => {
+              setProjectID(selected?.id ?? null);
+              setProjModalVisible(false);
+            }}
           />
+          <View
+            className="overflow-hidden rounded-md pb-2"
+            onLayout={(e) => {
+              e.target.measure((x, y, width, height, pageX, pageY) => {
+                projButtonLayout.current = {
+                  x: pageX,
+                  y: pageY + 50,
+                  width,
+                  height: 0,
+                };
+              });
+            }}
+          >
+            <TouchableNativeFeedback onPress={() => setProjModalVisible(true)}>
+              <View className="h-13 flex-row items-center justify-between gap-4 rounded-md border-2 border-gray-500 p-2">
+                {project_id === null && (
+                  <Text className="text-gray-400">Select Project</Text>
+                )}
+                {project_id !== null && (
+                  <>
+                    <View
+                      className="aspect-square h-full items-center justify-center rounded-full"
+                      style={{
+                        backgroundColor: projectsQuery.data?.find(
+                          (p) => p.id === project_id,
+                        )?.color,
+                      }}
+                    >
+                      <Icon
+                        name={
+                          projectsQuery.data?.find((p) => p.id === project_id)
+                            ?.icon as any
+                        }
+                        color={"white"}
+                        size={16}
+                      />
+                    </View>
+                    <Text className="flex-grow text-gray-800">
+                      {
+                        projectsQuery.data?.find((p) => p.id === project_id)
+                          ?.name
+                      }
+                    </Text>
+                  </>
+                )}
+                <Icon
+                  name="material/keyboard-arrow-down"
+                  color="#444444"
+                  size={24}
+                />
+              </View>
+            </TouchableNativeFeedback>
+          </View>
           <MyTagInput
             placeholder="Tags"
             value={tags}
